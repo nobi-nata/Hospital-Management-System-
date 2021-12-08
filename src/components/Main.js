b8cff18318202ad152e338c176c4ada4c4a89aa6
Laxmikant <85544667+nobi-nata@users.noreply.github.com>
2021-12-08 18:42:19 +0530

    dec_8 2nd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Carausel from "./components/Carausel";
-import Card from "./components/Card";
-import Details from "./components/Details";
+import Carausel1 from "./Carausel1";
+import Card from "./Card";
+import Details from "./Details";
 
 const Main = () => {
   return (
     <div>
-      <div className="container" my-3>
-        <Carausel />
+      <div className="container mb-3 ">
+        <Carausel1 />
       </div>
       <div className="container" my-5>
         <Details />
